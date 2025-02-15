8c6c3e4dc93bf7728f1a04a94f8e66dfca517345
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2025-02-15 21:55:36 +0900

    terminate ec2 instance

---
+++ Boto3 project/terminate_ec2instance.py
@@ -0,0 +1,10 @@
+import boto3
+
+aws_management_console = boto3.session.Seeion(profile_name="default")
+
+ec2_console = aws_management_console.client('ec2')
+
+#documentation: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/stop_instances.html
+response = ec2_console.terminate_instances(
+    InstanceIds=[''],#specify ec2 instance id that you want to stop running.
+)
