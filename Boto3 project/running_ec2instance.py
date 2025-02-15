cc1a0d604f0026f2d82ead2aca07d1e09e1cccd5
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2025-02-15 21:04:07 +0900

    create runninng ec2 instance automatically

--- Boto3 project/running_ec2instance.py
+++ Boto3 project/running_ec2instance.py
@@ -4,6 +4,8 @@ import boto3
 aws_management_console = boto3.session.Session(profile_name="default")
 #Open EC2 Console
 ec2_console = aws_management_console.client(service_name = 'ec2')
-#Use boto3 documentation to get more information
-result = ec2_console.describe_instance()
-print(result)
+#Use boto3 documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_instances.html)
+result = ec2_console.describe_instance()['Reservation']
+for each_instance in result:
+    for value in each_instance['Incetances']:
+        print(value['Instances'])
